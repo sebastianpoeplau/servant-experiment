02a223c58d18949307b07b15d5c3b70fd42dd09b
Sebastian Poeplau <sebastian.poeplau@googlemail.com>
2016-01-07 08:54:35 +0100

    First prototype of the scraper.

--- src/Scrape.hs
+++ src/Scrape.hs
@@ -1,8 +1,32 @@
-module Scrape () where
+{-# LANGUAGE OverloadedStrings #-}
+module Scrape (getArticles) where
 
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Maybe
+import Flow
 import Network.HTTP (getRequest, getResponseBody, simpleHTTP)
+import Text.HTML.TagSoup
+
+
+data Article = Article { title :: Text
+                       , url :: Text
+                       }
 
 
 -- | Download a web page.
-getPage :: String -> IO String
-getPage url = simpleHTTP (getRequest url) >>= getResponseBody
+getPage :: Text -> IO Text
+getPage url = do
+    response <- url |> T.unpack .> getRequest .> simpleHTTP
+    T.pack <$> getResponseBody response
+
+
+-- | Extract a list of articles from Bunte.
+getArticles :: IO [Article]
+getArticles = do
+    page <- getPage "www.bunte.de"
+    page |> parseTags .> filter (~== ("<article>" :: String)) .> map toArticle .> catMaybes .> return
+    where
+        toArticle (TagOpen _ attrs) = Article <$> lookup "data-article-title" attrs
+                                              <*> lookup "data-internal-url" attrs
+        toArticle _                 = Nothing

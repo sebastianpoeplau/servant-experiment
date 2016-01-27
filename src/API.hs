e3ed8be937fba83597c6f9e94c180924da7a9dce
Sebastian Poeplau <sebastian.poeplau@googlemail.com>
2016-01-27 19:32:41 +0100

    Give handlers access to a database

--- src/API.hs
+++ src/API.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
 module API
     ( startApp
     ) where
 
 import           Control.Monad.IO.Class
+import           Control.Monad.Logger     (runStderrLoggingT)
+import           Database.Persist.Sql     (SqlBackend, runMigration, runSqlConn)
+import           Database.Persist.Sqlite  (withSqliteConn)
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
@@ -16,13 +20,14 @@ import           Types
 type API = "articles" :> Get '[JSON] [Article]
 
 startApp :: IO ()
-startApp = run 8080 app
+startApp = runStderrLoggingT $ withSqliteConn ":memory:" $ \connection -> do
+  runSqlConn (runMigration migrateAll) connection
+  liftIO $ run 8080 (app connection)
 
-app :: Application
-app = serve api server
+app :: SqlBackend -> Application
+app connection = serve (Proxy :: Proxy API) (server connection)
 
-api :: Proxy API
-api = Proxy
+server :: SqlBackend -> Server API
+server = getArticlesHandler
 
-server :: Server API
-server = liftIO getArticles
+getArticlesHandler connection = liftIO getArticles
